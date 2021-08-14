59cbdd52b813592c45e7a8779e7af22e3d4949d4
Kevin98 <kevinryner@yahoo.co.uk>
2021-08-14 13:37:45 +0100

    manager.test tests all pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,21 +1,23 @@
-const manager = require("../classes/manager");
+const Manager = require("../classes/manager");
 
 describe('Create new manager', () => {
     it('Create a new manager using constructors name, email, id and office number', () => {
-      const manager = new Manager(`Kevin`, 23, `Kevin@ryner.com`, 2);
+      const manager = new Manager(`Matt`, 17, `Matt@fox.com`, 2);
 
-      expect(employee.empName).toEqual("Matt");
-      expect(employee.empID).toEqual(17);
-      expect(employee.email).toEqual("Matt@fox.com");
+      expect(manager.empName).toEqual("Matt");
+      expect(manager.empID).toEqual(17);
+      expect(manager.email).toEqual("Matt@fox.com");
+      expect(manager.officeNumber).toEqual(2);
+
     });
 
-    it('tests the methods on the created class to see it they return values expected', () => {
-        const employee = new Employee(`Kevin`, 23, `Kevin@ryner.com`);
+    it('tests the methods on the manager class to see it they return values expected', () => {
+        const manager = new Manager(`Matt`, 17, `Matt@fox.com`, 2);
   
-        expect(employee.getName()).toEqual("Kevin");
-        expect(employee.getId()).toEqual(23);
-        expect(employee.getEmail()).toEqual("Kevin@ryner.com");
-        expect(employee.getRole()).toEqual("Employee");
+        expect(manager.getName()).toEqual("Matt");
+        expect(manager.getId()).toEqual(17);
+        expect(manager.getEmail()).toEqual("Matt@fox.com");
+        expect(manager.getRole()).toEqual("Manager");
       });
 
 })
